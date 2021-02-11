816ca6d91f0990329abe593d4fd7669dcc4aee06
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-11 16:14:26 -0500

    set TZ before running Cypress

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,9 +1,15 @@
 /// <reference types="cypress" />
+
+// get the current time zone name
+const timeZone = Intl.DateTimeFormat()
+  .resolvedOptions()
+  .timeZone.replace('/', '-')
+
 describe('Page', () => {
   it('shows the timestamp', () => {
     cy.visit('index.html')
     cy.get('#timezone').should('be.visible').and('not.be.empty')
     cy.get('#local-time').should('be.visible').and('not.be.empty')
-    cy.screenshot()
+    cy.screenshot(`timestamp-${timeZone}`)
   })
 })
---
+++ test-timezones.js
@@ -0,0 +1,38 @@
+// we want to test the following timezones
+const timeZones = [
+  `America/New_York`,
+  `Europe/Berlin`,
+  `Europe/London`,
+  `Asia/Tokyo`,
+]
+
+// https://on.cypress.io/module-api
+const cypress = require('cypress')
+
+const runTests = (timeZone) => {
+  process.env.TZ = timeZone
+
+  // we can remove cypress/screenshots ourselves
+  return cypress.run({
+    config: {
+      trashAssetsBeforeRuns: false,
+    },
+  })
+}
+
+async function testTimeZones() {
+  for await (timeZone of timeZones) {
+    console.log('testing timezone %s', timeZone)
+    await runTests(timeZone)
+  }
+}
+
+testTimeZones().then(
+  () => {
+    console.log('all done')
+  },
+  (err) => {
+    console.error(err)
+    process.exit(1)
+  },
+)
