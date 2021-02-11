0f14422c7a083d10d7d04a92e6215127e8b01015
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-11 15:52:55 -0500

    show local time

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # test-timezones-example
 > Example of testing the same browser page in different timezones
+
+## Timezones
+
+- `America/New_York`
+- `Europe/Berlin`
+- `Europe/London`
+- `Asia/Tokyo`
+
+To run Cypress test in different timezone, use
+
+```shell
+$ <timezone> npx cypress open
+# for example
+$ TZ=Asia/Tokyo npx cypress open
+```
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,8 +2,8 @@
 describe('Page', () => {
   it('shows the timestamp', () => {
     cy.visit('index.html')
-    cy.get('#lang').should('be.visible').and('not.be.empty')
-    cy.get('output').should('be.visible').and('not.be.empty')
+    cy.get('#timezone').should('be.visible').and('not.be.empty')
+    cy.get('#local-time').should('be.visible').and('not.be.empty')
     cy.screenshot()
   })
 })
