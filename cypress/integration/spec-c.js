d3c93898f3b4f409073d86fb1f3c973e26d872e0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-12 11:21:52 -0500

    cloning specs

--- cypress/integration/spec-c.js
+++ cypress/integration/spec-c.js
@@ -5,12 +5,12 @@ const timeZone = Intl.DateTimeFormat()
   .resolvedOptions()
   .timeZone.replace('/', '-')
 
-describe('Page A', () => {
+describe('Page C', () => {
   it('shows the timestamp', () => {
     cy.visit('index.html')
     cy.get('#timezone').should('be.visible').and('not.be.empty')
     cy.get('#local-time').should('be.visible').and('not.be.empty')
-    cy.screenshot(`timestamp-A-${timeZone}`)
+    cy.screenshot(`timestamp-C-${timeZone}`)
     cy.wait(10000)
   })
 })
